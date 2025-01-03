de5219b0b957e3b4d6e65d1b3fef17e06f3c519a
acapple <acapple2002@gmail.com>
2025-01-03 13:55:43 -0500

    Added basics of networking

---
+++ Assets/Scripts/NetworkHud.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+
+public class NetworkHud : MonoBehaviour
+{
+    private void OnGUI()
+    {
+        GUILayout.BeginArea(new Rect(10, 10, 200, 200));
+        if (!NetworkManager.Singleton.IsClient && !NetworkManager.Singleton.IsServer)
+        {
+            StartButtons();
+        } else
+        {
+            StatusLabels();
+        }
+    }
+
+    static void StartButtons()
+    {
+        if (GUILayout.Button("Host"))
+        {
+            NetworkManager.Singleton.StartHost();
+        }
+        if (GUILayout.Button("Client"))
+        {
+            NetworkManager.Singleton.StartClient();
+        }
+        if (GUILayout.Button("Server"))
+        {
+            NetworkManager.Singleton.StartServer();
+        }
+    }
+
+    static void StatusLabels()
+    {
+        string mode;
+        if (NetworkManager.Singleton.IsHost)
+            mode = "Host";
+        else if (NetworkManager.Singleton.IsServer)
+            mode = "Server";
+        else
+            mode = "Unknown";
+        GUILayout.Label("Transport: " + NetworkManager.Singleton.NetworkConfig.NetworkTransport.GetType().Name);
+        GUILayout.Label("Mode: " + mode);
+        if (NetworkManager.Singleton.IsServer)
+            GUILayout.Label("# Connected: " + NetworkManager.Singleton.ConnectedClients.Count);
+    }
+}
