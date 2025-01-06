370a2785e3997b61a111e860b6d0520cf23cdac3
acapple <acapple2002@gmail.com>
2025-01-06 12:00:39 -0500

    Started work on the cards

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -2,12 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Card : MonoBehaviour
+public abstract class Card : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+
+    protected virtual void OnPlay()
+    {
+        
+    }
+
+    protected virtual void OnDraw()
+    {
+
+    }
+
+    protected virtual void OnDiscard()
+    {
+
     }
 
     // Update is called once per frame
