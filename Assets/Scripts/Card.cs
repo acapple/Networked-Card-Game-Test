d1148cff3de6efec309d78359364be906d62ee68
acapple <acapple2002@gmail.com>
2025-01-22 20:03:25 -0500

    Added a class for the card variables

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Card : MonoBehaviour
+public abstract class Card 
 {
     // Start is called before the first frame update
-    void Start()
+    void Initialize()
     {
         
     }
@@ -23,11 +23,5 @@ public abstract class Card : MonoBehaviour
     protected virtual void OnDiscard()
     {
 
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
