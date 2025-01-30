d693bd6fdb32d0d8e9b1829a6ee74bc71f6f40c1
acapple <acapple2002@gmail.com>
2025-01-30 13:17:34 -0500

    Players are dealt cards

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : Target
 {
@@ -8,16 +9,36 @@ public class Player : Target
     List<Card> hand;
     [SerializeField]
     Deck deck;
+    [SerializeField]
+    GameObject cardHandSample;
+    [SerializeField]
+    Canvas canvas;
 
     // Start is called before the first frame update
     void Start()
     {
         deck.ShuffleDeck();
+        for (int i=0; i<5; i++)
+        {
+            hand.Add(deck.drawCard());
+        }
+        SortHand();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    internal void SortHand()
+    {
+        int CardWidth = (int) Mathf.Floor(canvas.pixelRect.width / hand.Count);
+        for (int i=0; i<hand.Count; i++)
+        {
+            GameObject temp = Instantiate(cardHandSample, new Vector3(i * CardWidth, (int)canvas.pixelRect.height / 2, 0), Quaternion.identity, canvas.transform);
+            temp.GetComponent<Image>().color = new Color(Random.Range(0.0f, 1), Random.Range(0.0f, 1), Random.Range(0.0f, 1));
+            temp.GetComponent<RectTransform>().sizeDelta = new Vector2(CardWidth, 50);
+        }
     }
 }
